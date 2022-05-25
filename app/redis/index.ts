7a8509121df4a66dfc6ba9d87ecbaa0280215c72
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2022-05-25 16:41:20 +0100

    redis update

--- app/redis/index.ts
+++ app/redis/index.ts
@@ -1,6 +1,8 @@
 import { createClient } from "redis";
 
-const redisClient = createClient();
+const redisClient = createClient({
+  url: "https://salty-forest-52253.herokuapp.com/",
+});
 
 redisClient.connect();
 
