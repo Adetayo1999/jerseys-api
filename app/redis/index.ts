4d67d03fff53b53a15c48bd9c03490013b9f055e
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2022-05-26 07:56:39 +0100

    Adding env variable for the redis url

--- app/redis/index.ts
+++ app/redis/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  url: "redis://default:CXKYbT0PP2UKvjAEKTmee55bHRESEMtB@redis-13742.c273.us-east-1-2.ec2.cloud.redislabs.com:13742",
+  url: process.env.REDIS_URL,
 });
 
 redisClient.connect();
